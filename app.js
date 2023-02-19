92a75ca97d2ba2fdf0c28140755dae0a8154c175
OmidRashidiFam <omidRashidiFam@gmail.com>
2023-02-19 23:30:37 +0330

    create person component and render it to the page

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
-const App = () => {
-  return React.createElement("dev", { className: "app" }, [
-    React.createElement(
-      "h1",
-      { className: "title" },
-      "React.js i   s Rendered!!!"
-    ),
+const Person = (props) => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, props.name),
+    React.createElement("h3", {}, `Is: ${props.accupation}`),
   ]);
 };
 
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+const App = () => {
+  return React.createElement("div", { className: "app" }, [
+    React.createElement(
+      "h1",
+      { className: "title" },
+      "React.js is Rendered!!!"
+    ),
+    React.createElement(
+      Person,
+      { name: "Omid", accupation: "Front-end Developer" },
+      null
+    ),
+    React.createElement(Person, { name: "Navid", accupation: "Student" }, null),
+  ]);
+};
+
+const container = document.getElementById("root");
+const root = ReactDOM.createRoot(container);
+root.render(React.createElement(App));
